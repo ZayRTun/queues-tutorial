0c24cbb81361b479327362b68ba88d6e76a45e78
Zayar Tun <zay.t@hotmail.com>
2021-07-30 19:35:23 +0630

    first commit

---
+++ app/Jobs/ReconcileAccount.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class ReconcileAccount implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $user;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        // throw new \Exception('Whoops!');
+
+        logger('Reconcile the user: ' . $this->user->name);
+    }
+}
